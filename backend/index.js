8242894d50131c0d5f9e7d947565dc3581ee13e0
Bhumik Virmani <virmanibhumik17@gmail.com>
2025-04-29 19:23:33 +0530

    index changes backend

--- backend/index.js
+++ backend/index.js
@@ -19,14 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// const corsOptions = {
-//     origin: ['http://localhost:5173'],
-//     credentials: true
-// };
 const corsOptions = {
-    origin: '*',
+    origin: ['http://localhost:5173', 'https://job-portal-sql.vercel.app/'],
     credentials: true
 };
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true
+// };
 
 app.use(cors(corsOptions));
 
