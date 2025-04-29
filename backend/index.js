2568c97fb8635767d18190f923ebf8fde818740e
Bhumik Virmani <virmanibhumik17@gmail.com>
2025-04-29 18:42:27 +0530

    testing origin

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// const corsOptions = {
+//     origin: ['http://localhost:5173'],
+//     credentials: true
+// };
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: '*',
     credentials: true
 };
+
 app.use(cors(corsOptions));
 
 app.use('/api/v1/user', userRoute);
